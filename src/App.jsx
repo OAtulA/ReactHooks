d8f5b1b731d1fc441476705ca7a3850c0c42ca07
OAtulA <oraonatula@gmail.com>
2023-10-10 19:14:28 +0530

    Made some change in the bg color for elements.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    
+   <div>
+    <h1>{count}</h1>
+    <button onClick={()=>setCount(count+1)}>Increment Button</button>
+   </div> 
   )
 }
 

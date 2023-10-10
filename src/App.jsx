19cb26a01edd5bf6a21adff73e874bef35708ab9
OAtulA <oraonatula@gmail.com>
2023-10-10 19:37:20 +0530

    Learnt about the way useState has a callBack function and its use.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import { useState, useEffect, useCallback, useMemo } from 'react'
 import './App.css'
 
+function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+    </div>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
+  console.log("re-Rendered")
+
+  const increment = () => {
+    setCount(previousCount =>previousCount + 1)
+    setCount(previousCount =>previousCount + 1)
+    setCount(previousCount =>previousCount + 1)
+  }
+
   return (
-   <div>
-    <h1>{count}</h1>
-    <button onClick={()=>setCount(count+1)}>Increment Button</button>
-   </div> 
+    <div>
+      <h1>{count}</h1>
+      <Contact></Contact>
+      <button onClick={increment}>Increment Button</button>
+    </div>
   )
 }
 
